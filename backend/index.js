a798b4be81edd389e39aca3d1f53541bf378d9e5
Janmejay-Pandya <jay110504@gmail.com>
2024-12-26 22:34:48 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -14,22 +14,19 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+app.use(cookieParser());
+app.use(express.json()); 
 app.use(cors({
     origin: 'http://localhost:5173', // Your frontend's URL
     credentials: true,
 }));
-app.use(cookieParser());
-// app.use(express.json());
-app.use(express.json({ limit: '10mb' })); // Increase the limit to 10MB
-app.use(express.urlencoded({ limit: '10mb', extended: true })); // For URL-encoded data
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "index.html"));
     });
 }
 
