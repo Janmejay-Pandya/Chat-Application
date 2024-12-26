dc1d5e54fd66d2306cbd5c37a5b6486231e73e3d
Janmejay-Pandya <jay110504@gmail.com>
2024-12-26 22:40:20 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(cookieParser());
 app.use(express.json()); 
+app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173', // Your frontend's URL
     credentials: true,
